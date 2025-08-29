3deee99c7e694f5c8cda867f0c9a7fcec5d5581f
AbrarHaider110 <abrarhaider104@gmail.com>
2025-08-29 20:10:40 +0500

    Updated

--- lib/Screens/profile.dart
+++ lib/Screens/profile.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:my_app/Screens/Loginscreen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -21,6 +22,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
   final TextEditingController passwordController = TextEditingController();
 
   bool isLoading = false;
+  bool isUpdatingProfile = false;
+  bool isLoggingOut = false;
   bool isDataLoaded = false;
   bool isPasswordVisible = false;
   File? profileImage;
@@ -61,7 +64,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
         return;
       }
 
-      // Call the GetUser API
       final response = await http.get(
         Uri.parse('https://lokate.bsite.net/api/user/GetUser?userId=$userId'),
         headers: {
@@ -108,7 +110,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   void handleLogout() async {
     setState(() {
-      isLoading = true;
+      isLoggingOut = true;
     });
 
     try {
@@ -122,14 +124,14 @@ class _ProfileScreenState extends State<ProfileScreen> {
       );
     } catch (e) {
       setState(() {
-        isLoading = false;
+        isLoggingOut = false;
       });
     }
   }
 
   void handleSaveChanges() async {
     setState(() {
-      isLoading = true;
+      isUpdatingProfile = true;
     });
 
     try {
@@ -139,7 +141,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
       if (userId == null || token == null) {
         setState(() {
-          isLoading = false;
+          isUpdatingProfile = false;
         });
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('User not authenticated!')),
@@ -172,7 +174,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
           await prefs.setString('contact', contactController.text);
 
           setState(() {
-            isLoading = false;
+            isUpdatingProfile = false;
           });
 
           ScaffoldMessenger.of(context).showSnackBar(
@@ -184,7 +186,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
           );
         } else {
           setState(() {
-            isLoading = false;
+            isUpdatingProfile = false;
           });
 
           ScaffoldMessenger.of(context).showSnackBar(
@@ -197,7 +199,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
         }
       } else if (response.statusCode == 404) {
         setState(() {
-          isLoading = false;
+          isUpdatingProfile = false;
         });
 
         ScaffoldMessenger.of(
@@ -205,7 +207,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
         ).showSnackBar(const SnackBar(content: Text('User not found!')));
       } else {
         setState(() {
-          isLoading = false;
+          isUpdatingProfile = false;
         });
 
         ScaffoldMessenger.of(context).showSnackBar(
@@ -216,7 +218,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
       }
     } catch (e) {
       setState(() {
-        isLoading = false;
+        isUpdatingProfile = false;
       });
 
       ScaffoldMessenger.of(
@@ -330,200 +332,213 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
 
-    if (isLoading) {
-      return const Scaffold(
-        body: SafeArea(child: Center(child: CircularProgressIndicator())),
+    if (isLoading && !isDataLoaded) {
+      return Scaffold(
+        body: Center(
+          child: LoadingAnimationWidget.inkDrop(
+            color: const Color(0xFF1A9C8C),
+            size: 60,
+          ),
+        ),
       );
     }
 
-    final content = Padding(
-      padding: EdgeInsets.symmetric(
-        horizontal: screenWidth * 0.06,
-        vertical: 12,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(height: 8),
-          Center(
-            child: Stack(
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(2),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(color: Colors.black, width: 2),
-                  ),
-                  child: CircleAvatar(
-                    radius: screenWidth * 0.14,
-                    backgroundColor: Colors.white,
-                    backgroundImage:
-                        profileImage != null ? FileImage(profileImage!) : null,
-                    child:
-                        profileImage == null
-                            ? Icon(
-                              Icons.person,
-                              size: screenWidth * 0.28,
-                              color: Colors.grey[700],
-                            )
-                            : null,
-                  ),
-                ),
-                Positioned(
-                  bottom: 4,
-                  right: 4,
-                  child: GestureDetector(
-                    onTap: showImageSourceActionSheet,
-                    child: Container(
-                      height: 26,
-                      width: 26,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        gradient: const LinearGradient(
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                          stops: [0.001, 1.0],
-                          colors: [
-                            Color(0xFF1A9C8C),
-                            Color.fromARGB(255, 2, 51, 164),
-                          ],
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom + 20,
+          ),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(minHeight: screenHeight),
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: screenWidth * 0.06,
+                vertical: 12,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 8),
+                  Center(
+                    child: Stack(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(2),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(color: Colors.black, width: 2),
+                          ),
+                          child: CircleAvatar(
+                            radius: screenWidth * 0.14,
+                            backgroundColor: Colors.white,
+                            backgroundImage:
+                                profileImage != null
+                                    ? FileImage(profileImage!)
+                                    : null,
+                            child:
+                                profileImage == null
+                                    ? Icon(
+                                      Icons.person,
+                                      size: screenWidth * 0.28,
+                                      color: Colors.grey[700],
+                                    )
+                                    : null,
+                          ),
                         ),
-                        border: Border.all(color: Colors.white, width: 2),
-                      ),
-                      child: const Icon(
-                        Icons.add,
-                        size: 14,
-                        color: Colors.white,
-                      ),
+                        Positioned(
+                          bottom: 4,
+                          right: 4,
+                          child: GestureDetector(
+                            onTap: showImageSourceActionSheet,
+                            child: Container(
+                              height: 26,
+                              width: 26,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                gradient: const LinearGradient(
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
+                                  stops: [0.001, 1.0],
+                                  colors: [
+                                    Color(0xFF1A9C8C),
+                                    Color.fromARGB(255, 2, 51, 164),
+                                  ],
+                                ),
+                                border: Border.all(
+                                  color: Colors.white,
+                                  width: 2,
+                                ),
+                              ),
+                              child: const Icon(
+                                Icons.add,
+                                size: 14,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 16),
-          buildTextField(
-            label: "Full Name",
-            controller: fullNameController,
-            enabled: true,
-          ),
-          buildTextField(
-            label: "Username",
-            controller: userNameController,
-            enabled: true,
-          ),
-          buildTextField(
-            label: "Contact",
-            controller: contactController,
-            enabled: true,
-          ),
-          buildTextField(
-            label: "Email",
-            controller: emailController,
-            enabled: false,
-          ),
-          // buildTextField(
-          //   label: "Password",
-          //   controller: passwordController,
-          //   isPasswordField: true,
-          //   enabled: false,
-          // ),
-          const SizedBox(height: 20),
-          Row(
-            children: [
-              Expanded(
-                child: InkWell(
-                  onTap: handleSaveChanges,
-                  borderRadius: BorderRadius.circular(10),
-                  child: Ink(
-                    decoration: const BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                        stops: [0.001, 1.0],
-                        colors: [
-                          Color(0xFF1A9C8C),
-                          Color.fromARGB(255, 2, 51, 164),
-                        ],
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                    ),
-                    child: const SizedBox(
-                      height: 45,
-                      child: Center(
-                        child: Text(
-                          "Update Profile",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15,
+                  const SizedBox(height: 16),
+                  buildTextField(
+                    label: "Full Name",
+                    controller: fullNameController,
+                    enabled: true,
+                  ),
+                  buildTextField(
+                    label: "Username",
+                    controller: userNameController,
+                    enabled: true,
+                  ),
+                  buildTextField(
+                    label: "Contact",
+                    controller: contactController,
+                    enabled: true,
+                  ),
+                  buildTextField(
+                    label: "Email",
+                    controller: emailController,
+                    enabled: false,
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: InkWell(
+                          onTap: isUpdatingProfile ? null : handleSaveChanges,
+                          borderRadius: BorderRadius.circular(10),
+                          child: Ink(
+                            decoration: BoxDecoration(
+                              gradient: const LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                stops: [0.001, 1.0],
+                                colors: [
+                                  Color(0xFF1A9C8C),
+                                  Color.fromARGB(255, 2, 51, 164),
+                                ],
+                              ),
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                              color: isUpdatingProfile ? Colors.grey : null,
+                            ),
+                            child: SizedBox(
+                              height: 45,
+                              child: Center(
+                                child:
+                                    isUpdatingProfile
+                                        ? LoadingAnimationWidget.inkDrop(
+                                          color: Colors.white,
+                                          size: 24,
+                                        )
+                                        : const Text(
+                                          "Update Profile",
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 15,
+                                          ),
+                                        ),
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(width: 16),
-              Expanded(
-                child: InkWell(
-                  onTap: handleLogout,
-                  borderRadius: BorderRadius.circular(10),
-                  child: Ink(
-                    decoration: const BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                        stops: [0.001, 1.0],
-                        colors: [
-                          Color(0xFF1A9C8C),
-                          Color.fromARGB(255, 2, 51, 164),
-                        ],
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: InkWell(
+                          onTap: isLoggingOut ? null : handleLogout,
+                          borderRadius: BorderRadius.circular(10),
+                          child: Ink(
+                            decoration: BoxDecoration(
+                              gradient: const LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                stops: [0.001, 1.0],
+                                colors: [
+                                  Color(0xFF1A9C8C),
+                                  Color.fromARGB(255, 2, 51, 164),
+                                ],
+                              ),
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                              color: isLoggingOut ? Colors.grey : null,
+                            ),
+                            child: SizedBox(
+                              height: 45,
+                              child: Center(
+                                child:
+                                    isLoggingOut
+                                        ? LoadingAnimationWidget.inkDrop(
+                                          color: Colors.white,
+                                          size: 24,
+                                        )
+                                        : const Text(
+                                          "Log out",
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 15,
+                                          ),
+                                        ),
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                    ),
-                    child: SizedBox(
-                      height: 45,
-                      child: Center(
-                        child:
-                            isLoading
-                                ? const CircularProgressIndicator(
-                                  color: Colors.white,
-                                )
-                                : const Text(
-                                  "Log out",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 15,
-                                  ),
-                                ),
-                      ),
-                    ),
+                    ],
                   ),
-                ),
+                  const SizedBox(height: 10),
+                ],
               ),
-            ],
+            ),
           ),
-          const SizedBox(height: 10),
-        ],
-      ),
-    );
-
-    return Scaffold(
-      body: SafeArea(
-        top: true,
-        bottom: true,
-        left: true,
-        right: true,
-        minimum: const EdgeInsets.all(0),
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            return constraints.maxHeight < 700
-                ? SingleChildScrollView(child: content)
-                : content;
-          },
         ),
       ),
     );
